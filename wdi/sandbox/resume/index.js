e24ad0befeeb7dd30387a600c03ec46d52c12cd4
MJ Sandoval <salssedan@gmail.com>
2018-10-29 11:56:36 -0400

    resume

---
+++ wdi/sandbox/resume/index.js
@@ -0,0 +1,10 @@
+const fs = require("fs");
+
+fs.readFile(__dirname + "/info.json", "utf8", function(err, data) {
+  if (err) {
+    console.log(err);
+  } else {
+    console.log(data);
+    return data;
+  }
+});
